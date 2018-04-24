877afe3f6cda321621c424882c799633e5f180fe
Your Name <you@example.com>
2018-04-24 16:04:35 +0000

    initial Oreo Trees

---
+++ product/init.mk
@@ -0,0 +1,14 @@
+
+# Init scripts
+PRODUCT_PACKAGES += \
+    fstab.qcom \
+    init.qcom.sh \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh \
+    init.target.rc \
+    init.rosy.rc \
+    ueventd.qcom.rc
+
+PRODUCT_PACKAGES += \
+    init.qcom.post_boot.sh
---
+++ product/lights.mk
@@ -0,0 +1,3 @@
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
+    lights.msm8953
